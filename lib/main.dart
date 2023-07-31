287c8c6f1077127d8830228c33ff390359ccdbcc
Lotis <guillaume.lotis@gmail.com>
2023-07-31 21:02:40 +0200

    WebSocket TCP working with the branch [websiteWorking] on the heroku project online [cube]. The node.js file is in the server folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,67 +36,86 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final channel = IOWebSocketChannel.connect('wss://cube-app-ded2c0470b54.herokuapp.com/');
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('WebSocket Client'),
+        ),
+        body: Center(
+          child: StreamBuilder(
+            stream: channel.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                String message = snapshot.data;
+                // Process the received message as needed
+                return Text('Received message from server: $message');
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void sendIMUData() async {
+    int i = 0;
+    Map<String, double> currentRotation = {
+      'gyro_x': 0.0,
+      'gyro_y': 0.0,
+      'gyro_z': 0.0,
+    };
+
+    while (true) {
+      Map<String, double> targetRotation = {
+        'gyro_x': Random().nextDouble() * 2 - 1.0,
+        'gyro_y': Random().nextDouble() * 2 - 1.0,
+        'gyro_z': Random().nextDouble() * 2 - 1.0,
+      };
+
+      int duration = 30;
+      for (int frame = 0; frame < duration; frame++) {
+        double t = frame / duration;
+        Map<String, double> easedRotation = {};
+        for (String axis in ['gyro_x', 'gyro_y', 'gyro_z']) {
+          easedRotation[axis] = (currentRotation[axis]! + (targetRotation[axis]! - currentRotation[axis]!) * t);
+        }
+
+        // Convert to JSON and send to the server
+        String message = json.encode(easedRotation);
+        channel.sink.add(message);
+        print('Sending data $i');
+        i++;
+
+        // Wait for some time before sending the next frame
+        await Future.delayed(Duration(milliseconds: 20));
+      }
+
+      // Update the current rotation to the target rotation after the transition
+      currentRotation = targetRotation;
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    sendIMUData();
+  }
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
   }
 }
